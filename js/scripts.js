ff7629930f3786fc7baf1d04566d49a4d8932704
Avrohom Perl <info@avrohomperl.com>
2021-06-18 05:46:10 -0400

    Time to sleep, various fixes etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
 $('.more').on('click', function() {
-    $('.card-footer').slideToggle(100);
+    $('.card-footer').slideToggle(200);
+    $('#skills').slideToggle(200);
 })
+
+// Rotate the menu button when its pressed
+var rotation = 0;
+
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({ 'transform': 'rotate(' + degrees + 'deg)' });
+};
+
+$('#menu-button').click(function() {
+    rotation += 180;
+    $(this).rotate(rotation);
+});
+
+$(document).ready(function() {
+    // Anti email spam step to fight off evil devs who make web scrapers.
+    var r = 'antiemailspam';
+
+    $('.' + r).each(function() {
+        var $this = $(this),
+            value = new String($this.text());
+        value = value.replace('[' + r + ']', '@');
+        $this.replaceWith($('<a></a>').text(value).attr('href', 'mailto:' + value));
+    });
+
+    // Anti phone spam step to fight off evil devs who make web scrapers.
+    var r = 'antiphonespam';
+
+    $('.' + r).each(function() {
+        var $this = $(this),
+            value = new String($this.text());
+        value = value.replace('[' + r + ']', '.256.');
+        $this.replaceWith($('<a></a>').text(value).attr('href', 'mailto:' + value));
+    });
+});
