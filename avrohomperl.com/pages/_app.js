32108c374610ff532721d0d5662549e459d0cf97
Avrohom Perl <info@avrohomperl.com>
2021-07-01 23:41:43 -0400

    Add google analytics to site

--- avrohomperl.com/pages/_app.js
+++ avrohomperl.com/pages/_app.js
@@ -1,9 +1,31 @@
 import Head from "next/head";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import '../styles/custom.scss';
+import "../styles/custom.scss";
 import "../styles/globals.css";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/ga";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -15,7 +37,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-    </>
+    </> 
   );
 }
 
