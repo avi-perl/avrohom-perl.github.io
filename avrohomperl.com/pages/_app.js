0bad40678aacada95885670b1b7683822c46023b
Avrohom Perl <info@avrohomperl.com>
2021-07-18 12:06:43 -0400

    Add small.chat

--- avrohomperl.com/pages/_app.js
+++ avrohomperl.com/pages/_app.js
@@ -15,6 +15,15 @@ function MyApp({ Component, pageProps }) {
     const handleRouteChange = (url) => {
       ga.pageview(url);
     };
+
+    // Add chat
+    if (process.env.SMALL_CHAT_ENABLED) {
+      const chatTag = document.createElement("script");
+      chatTag.src = "https://embed.small.chat/T028BAWJTRBC028BBS72G5.js";
+      chatTag.async = true;
+      document.body.appendChild(chatTag);
+    }
+
     //When the component is mounted, subscribe to router changes
     //and log those page views
     router.events.on("routeChangeComplete", handleRouteChange);
@@ -37,7 +46,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-    </> 
+    </>
   );
 }
 
